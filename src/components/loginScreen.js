9ddf0bb60f847c9372a2a46237250d39826b047c
Michał Kiełbasa <michal.kielbasa1994@gmail.com>
2023-02-19 21:01:00 +0100

    Build fix

--- src/components/loginScreen.js
+++ src/components/loginScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { useState } from "react";
 import {RxCross2} from 'react-icons/rx'
 import {SiGmail} from 'react-icons/si'
 import {AiOutlineFacebook, AiFillGithub} from 'react-icons/ai'
+import  Link from 'next/link'
 
 const LoginScreen = ({loginHandle}) => {
 
@@ -13,18 +13,20 @@ const LoginScreen = ({loginHandle}) => {
           <h2>Log in to Blog</h2>
           <form>
             <input placeholder="Login"></input>
-            <input placeholder="Password"  type="password"></input>
-            
-            <button type="submit" onClick={(e) => e.preventDefault()}>Login</button>
+            <input placeholder="Password" type="password"></input>
+
+            <button type="submit" onClick={(e) => e.preventDefault()}>
+              Login
+            </button>
           </form>
           <p>
-            Don't have an account yet? <a href="/">Sign up!</a>
+            Don&apos;t have an account yet? <Link href="/">Sign up!</Link>
           </p>
           <p>Log in with:</p>
           <div className="loginIcons">
             <AiOutlineFacebook size={50} className="icon" />
             <AiFillGithub size={50} className="icon" />
-            <SiGmail size={50} className="icon"/>
+            <SiGmail size={50} className="icon" />
           </div>
         </div>
       </div>
