6c2aa1933f3ba8521db88adebd1fea305dcb597d
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-03-11 22:15:11 -0400

    Fixed footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,10 +18,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      
-      <MainGrid />
-      <Footer />
+      <body>
+        <Header />
+
+        <MainGrid />
+        <Footer />
+      </body>
     </>
   );
 }
